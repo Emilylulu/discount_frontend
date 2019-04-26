d815ca21170a600fd3a645155f0bb203c8efffa6
yangyu1009 <>
2019-04-26 13:23:13 -0700

    update item page

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+// MDB Angular Free
+import { WavesModule, ButtonsModule, IconsModule } from 'angular-bootstrap-md';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
