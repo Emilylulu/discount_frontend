4349e41a5c9b6e1942610623b407f0fb00802d07
yangyu1009 <>
2019-04-30 15:35:52 -0700

    get one item from database by asin

--- src/app/constants/endpoint.ts
+++ src/app/constants/endpoint.ts
@@ -1,7 +1,8 @@
 const BASE_URL = 'http://localhost:5005';
 
 const endpoints = {
-  ALL_PRODUCT: BASE_URL + '/cloth/all'
+  ALL_PRODUCT: BASE_URL + '/cloth/all',
+  ONE_ITEM: BASE_URL + '/cloth/user/8921463267'
 };
 
 export default endpoints;
--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+
+import axios from 'axios';
+import Cloth from './model/item';
+import endpoints from '../constants/endpoint';
+import { detectChangesInternal } from '@angular/core/src/render3/instructions';
+import { detachEmbeddedView } from '@angular/core/src/view';
+
+
+
 declare var $:any;
 @Component({
   selector: 'app-item',
@@ -18,10 +27,59 @@ export class ItemComponent implements OnInit {
 
 
     }
-
+    list;
     constructor() { }
+    
+    
+    async ngOnInit() {
+      try {
+        this.list = await axios.get(endpoints.ONE_ITEM).then(function(response){
+          
+            var detail;
+            detail = response.data;
+            console.log(response.data.title);
+            
+            return detail;
 
-    ngOnInit() {
+        }
+        
+          
+
+        );
+      } catch (e) {
+        // TODO handle get data fail later
+        console.table(`Error connecting with server: ${e}`);
+      }
+      console.log(this.list.title);
+      
     }
+
+    
+      /*const user = axios.get(endpoints.ONE_ITEM)
+      .then(function (response) {
+        //console.log(response);
+        //this.productdata = response.data.title;
+        //console.log(response.data);
+        this.list = response.data;
+        console.log(this.list);
+
+        return this.list;
+        
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      */
+
+      
+
+      
+      
+      
+
   
-  }
+   
+  
+  
+}
